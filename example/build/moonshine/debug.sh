f77800e889cfb0f89bc851075fe8804494cd9a43
Samuel Baird <samuelwbaird@gmail.com>
2016-03-07 23:45:41 +1100

    tidy up

--- example/build/moonshine/debug.sh
+++ example/build/moonshine/debug.sh
@@ -7,6 +7,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # ensure we're running from the correct folder
 cd $DIR
 
+# clean up old debug processes?
+killall node
+killall python
+
 # moonshine debug server with the output path mapped to the combined source path
 moonshine debug -m "stones:." &
 
@@ -18,3 +22,8 @@ open "http://127.0.0.1:1969" &
 
 # open the game in another browser window
 open "http://localhost:8000/dist" &
+
+# wait for a key press and then tidy up again
+read -n 1 -s
+killall node
+killall python
